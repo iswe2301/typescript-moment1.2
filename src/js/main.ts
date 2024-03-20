fc30498510a259ff5cd4c4335217078ac66b3448
Isa Westling <isawestling.blogg@hotmail.com>
2024-03-20 14:59:18 +0100

    Uppdaterat displayCourses-funktionen för att kontrollera om det finns sparade kurser, annars döljs kurscontainern.

--- src/js/main.ts
+++ src/js/main.ts
@@ -5,6 +5,7 @@ const nameInput = document.getElementById("name") as HTMLInputElement; // som in
 const progressionSelect = document.getElementById("progression") as HTMLSelectElement; // som select-element
 const syllabusInput = document.getElementById("syllabus") as HTMLInputElement; // som input
 const tableEl = document.getElementById("courses") as HTMLTableSectionElement; // som table-section
+const coursesEl = document.getElementById("course-section") as HTMLElement;
 // Lagrar variabel för kurskod för att som hålla reda på vilken kurs som redigeras, tom sträng = ingen kurs redigeras
 let editCode: string = "";
 
@@ -116,62 +117,68 @@ function getCourses(): CourseInfo[] {
 // Funktion för att visa sparade kurser på webbsidan
 function displayCourses(): void {
     const courses = getCourses(); // Hämtar befintliga kurser från localStorage
-    tableEl.innerHTML = ""; // Rensar innehållet i tabellen
+    // Kontrollerar om det finns sparade kurser
+    if (courses.length > 0) {
+        coursesEl.style.display = "block"; // Visar container för kurslistan/tabellen
+        tableEl.innerHTML = ""; // Rensar innehållet i tabellen
 
-    // Loopar genom varje kurs
-    courses.forEach(course => {
+        // Loopar genom varje kurs
+        courses.forEach(course => {
 
-        // Skapar en ny tabellrad för varje kurs
-        const rowEl: HTMLTableRowElement = document.createElement("tr");
-        tableEl.appendChild(rowEl); // Lägger till den nya raden i tabellens body
+            // Skapar en ny tabellrad för varje kurs
+            const rowEl: HTMLTableRowElement = document.createElement("tr");
+            tableEl.appendChild(rowEl); // Lägger till den nya raden i tabellens body
 
-        // Skapar en ny cell för kurskod
-        const courseCodeEl: HTMLTableCellElement = document.createElement("td");
-        courseCodeEl.innerHTML = course.code; // Sätter cellens innehåll till kursens kod
-        rowEl.appendChild(courseCodeEl); // Lägger till cellen i raden
+            // Skapar en ny cell för kurskod
+            const courseCodeEl: HTMLTableCellElement = document.createElement("td");
+            courseCodeEl.innerHTML = course.code; // Sätter cellens innehåll till kursens kod
+            rowEl.appendChild(courseCodeEl); // Lägger till cellen i raden
 
-        // Skapar en ny cell för kursnamn
-        const courseNameEl: HTMLTableCellElement = document.createElement("td");
-        courseNameEl.innerHTML = course.name; // Sätter cellens innehåll till kursens namn
-        rowEl.appendChild(courseNameEl); // Lägger till cellen i raden
+            // Skapar en ny cell för kursnamn
+            const courseNameEl: HTMLTableCellElement = document.createElement("td");
+            courseNameEl.innerHTML = course.name; // Sätter cellens innehåll till kursens namn
+            rowEl.appendChild(courseNameEl); // Lägger till cellen i raden
 
-        // Skapar en ny cell för progression
-        const progressionEl: HTMLTableCellElement = document.createElement("td");
-        progressionEl.innerHTML = course.progression; // Sätter cellens innehåll till kursens progression
-        rowEl.appendChild(progressionEl); // Lägger till cellen i raden
+            // Skapar en ny cell för progression
+            const progressionEl: HTMLTableCellElement = document.createElement("td");
+            progressionEl.innerHTML = course.progression; // Sätter cellens innehåll till kursens progression
+            rowEl.appendChild(progressionEl); // Lägger till cellen i raden
 
-        // Skapar en cell för kursplanlänken
-        const syllabusEl: HTMLTableCellElement = document.createElement("td");
-        const link: HTMLAnchorElement = document.createElement("a"); // Skapar ett nytt a-element
-        link.href = course.syllabus; // Sätter länkens url till kursens syllabus
-        link.innerHTML = "Kursplan"; // Sätter länkens text
-        link.target = "_blank"; // Sätter target till öppnas i ny flik
-        syllabusEl.appendChild(link); // Lägger till länken i cellen
-        rowEl.appendChild(syllabusEl); // Lägger till cellen i raden
+            // Skapar en cell för kursplanlänken
+            const syllabusEl: HTMLTableCellElement = document.createElement("td");
+            const link: HTMLAnchorElement = document.createElement("a"); // Skapar ett nytt a-element
+            link.href = course.syllabus; // Sätter länkens url till kursens syllabus
+            link.innerHTML = "Kursplan"; // Sätter länkens text
+            link.target = "_blank"; // Sätter target till öppnas i ny flik
+            syllabusEl.appendChild(link); // Lägger till länken i cellen
+            rowEl.appendChild(syllabusEl); // Lägger till cellen i raden
 
-        // Skapar en cell för redigeringsknappen
-        const editEl: HTMLTableCellElement = document.createElement("td")
-        const editButton: HTMLButtonElement = document.createElement("button"); // Skapar en ny knapp
-        editButton.innerHTML = "Redigera"; // Sätter knappens innehåll
-        editButton.addEventListener("click", () => editCourse(course.code)); // Skapar en händelselyssnare som vid klick anropar funktion för att redigera kurs, skickar med kurskod som argument
-        editEl.appendChild(editButton); // Lägger till knappen i cellen
-        rowEl.appendChild(editEl); // Lägger till cellen i raden
+            // Skapar en cell för redigeringsknappen
+            const editEl: HTMLTableCellElement = document.createElement("td")
+            const editButton: HTMLButtonElement = document.createElement("button"); // Skapar en ny knapp
+            editButton.innerHTML = "Redigera"; // Sätter knappens innehåll
+            editButton.addEventListener("click", () => editCourse(course.code)); // Skapar en händelselyssnare som vid klick anropar funktion för att redigera kurs, skickar med kurskod som argument
+            editEl.appendChild(editButton); // Lägger till knappen i cellen
+            rowEl.appendChild(editEl); // Lägger till cellen i raden
 
-        // Skapa en cell för ta-bort knappen
-        const deleteEl: HTMLTableCellElement = document.createElement("td");
-        const deleteButton: HTMLButtonElement = document.createElement("button"); // Skapar en ny knapp
-        deleteButton.innerHTML = "Ta bort"; // Sätter knappens innehåll
-        deleteButton.addEventListener("click", () => deleteCourse(course.code)); // Skapar en händelselyssnare som vid klick anropar funktion för att radera kurs, skickar med kurskod som argument
-        deleteEl.appendChild(deleteButton); // Lägger till knappen i cellen
-        rowEl.appendChild(deleteEl); // Lägger till cellen i raden
-    });
+            // Skapa en cell för ta-bort knappen
+            const deleteEl: HTMLTableCellElement = document.createElement("td");
+            const deleteButton: HTMLButtonElement = document.createElement("button"); // Skapar en ny knapp
+            deleteButton.innerHTML = "Ta bort"; // Sätter knappens innehåll
+            deleteButton.addEventListener("click", () => deleteCourse(course.code)); // Skapar en händelselyssnare som vid klick anropar funktion för att radera kurs, skickar med kurskod som argument
+            deleteEl.appendChild(deleteButton); // Lägger till knappen i cellen
+            rowEl.appendChild(deleteEl); // Lägger till cellen i raden
+        });
+    } else {
+        coursesEl.style.display = "none"; // Döljer container för kurslistan/tabellen om inga kurser finns
+    }
 }
 
 // Funktion för att radera kurs baserat på kurskod
 function deleteCourse(courseCode: string): void {
     const courses = getCourses(); // Hämtar kurserna som finns sparade i localStorage
-    courses.filter(course => course.code !== courseCode); // Filtrerar ut alla kurser som inte matchar den aktuella kurskoden och skapar en ny lista
-    saveCourses(courses); // Sparar den uppdaterade listan av kurser till localStorage
+    let remainingCourses = courses.filter(course => course.code !== courseCode); // Filtrerar ut alla kurser som inte matchar den aktuella kurskoden och skapar en ny lista
+    saveCourses(remainingCourses); // Sparar den uppdaterade listan av kurser till localStorage
     displayCourses(); // Anropar funktion för att uppdatera kurserna som visas på webbsidan
 }
 
