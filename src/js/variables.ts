5f5f071714023a4e8b1f1b8e2cd17e8edd3e64e1
Isa Westling <isawestling.blogg@hotmail.com>
2024-03-21 14:03:39 +0100

    Skapat egen fil för lagrade variabler, ändrat variablerna till export (modul). Ändrat variabel för editCode till ett objekt för att kunna exportera korrekt och ändra värdet senare.

---
+++ src/js/variables.ts
@@ -0,0 +1,12 @@
+// Hämtar elementet genom ID, lagrar variabler för export
+export const formEl = document.getElementById("form-container") as HTMLFormElement; // som formulär
+export const codeInput = document.getElementById("code") as HTMLInputElement; // som input
+export const nameInput = document.getElementById("name") as HTMLInputElement; // som input
+export const progressionSelect = document.getElementById("progression") as HTMLSelectElement; // som select-element
+export const syllabusInput = document.getElementById("syllabus") as HTMLInputElement; // som input
+export const tableEl = document.getElementById("courses") as HTMLTableSectionElement; // som table-section
+export const coursesEl = document.getElementById("course-section") as HTMLElement;
+
+// Skapar ett objekt för att kunna exportera editCode och ändra värdet senare.
+// Lagrar variabel för kurskod för att som hålla reda på vilken kurs som redigeras, tom sträng = ingen kurs redigeras
+export const newCourseCode: { editCode: string } = { editCode: "" };
