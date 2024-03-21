c026cda39f200d24588ed7139634e94d19187925
Isa Westling <isawestling.blogg@hotmail.com>
2024-03-21 14:01:41 +0100

    Skapat egen fil för interface, ändrat till export (modul).

---
+++ src/js/interface.ts
@@ -0,0 +1,7 @@
+// Interface som definierar strukturen på en kurs, tar emot strängar (exporteras)
+export interface courseInfo {
+    code: string;
+    name: string;
+    progression: string;
+    syllabus: string;
+}
