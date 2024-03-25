85e21bd7b8a0b3c41c1be961acc7231e634bb942
Isa Westling <isawestling.blogg@hotmail.com>
2024-03-25 14:35:45 +0100

    lagt till variabel för popup och exporterat.

--- src/js/variables.ts
+++ src/js/variables.ts
@@ -5,7 +5,8 @@ export const nameInput = document.getElementById("name") as HTMLInputElement; //
 export const progressionSelect = document.getElementById("progression") as HTMLSelectElement; // som select-element
 export const syllabusInput = document.getElementById("syllabus") as HTMLInputElement; // som input
 export const tableEl = document.getElementById("courses") as HTMLTableSectionElement; // som table-section
-export const coursesEl = document.getElementById("course-section") as HTMLElement;
+export const coursesEl = document.getElementById("course-section") as HTMLElement; // som HTML-element
+export const popupEl = document.querySelector('.popup') as HTMLElement; // som HTML-element
 
 // Skapar ett objekt för att kunna exportera editCode och ändra värdet senare.
 // Lagrar variabel för kurskod för att som hålla reda på vilken kurs som redigeras, tom sträng = ingen kurs redigeras
